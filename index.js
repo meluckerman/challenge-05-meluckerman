85e6996955bb982a37dad35295f53ee64a990f35
luckerman09 <ricky.luckerman@gmail.com>
2022-06-05 17:33:11 +0700

    ch-sorting

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (list, fnSort) => {
+dataSort = fnSort(list);
+let a = 1;
+let arr = [];
+  for (i = 0; i < dataSort.length; i++){
+     arr.push((i + a + '.' + dataSort[i]));
+  }
+  return arr;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (list) => {
+  return list.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (list) => {
+  return list.reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
